bfad842a5df33d5d7c15768b9200704dcef09601
Tuan Nguyen <tuan.nguyen@eaera.com>
2024-03-30 15:13:56 +0700

    Add video table to the database schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_03_28_123340) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_30_134425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,4 +26,15 @@ ActiveRecord::Schema[7.0].define(version: 2024_03_28_123340) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "videos", force: :cascade do |t|
+    t.string "title"
+    t.string "description"
+    t.string "video_id"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_videos_on_user_id"
+  end
+
+  add_foreign_key "videos", "users"
 end
